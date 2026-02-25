880d54868e57832e1739d4bae1cc6264c9ce84bf
sngmn7 <lsm123727@gmail.com>
2026-02-25 16:38:39 +0900

    Solve: 백준 2438번 (별 찍기 - 1) - class1 정리 시작

---
+++ class1/2438.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int N;
+    cin >> N;
+
+    for(int i = 0; i<N ; i++){
+        for(int j = 0; j<=i ; j++){
+            cout << "*";
+        }
+        cout << "\n";
+    }
+}
