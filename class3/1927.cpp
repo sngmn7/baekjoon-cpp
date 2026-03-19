a2a1c254306b80948040e3397871ea33ca3d820e
sngmn7 <lsm123727@gmail.com>
2026-03-19 17:25:56 +0900

    Solve: BOJ 1927 - 최소 힙 구현 (class3)

---
+++ class3/1927.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    priority_queue<int, vector<int>, greater<int>> pq;
+
+    while (n--){
+        int x;
+        cin >> x;
+
+        if (x == 0){
+            if (pq.empty()){
+                cout << "0\n";
+            } else{
+                cout << pq.top() << "\n";
+                pq.pop();
+            }
+        } else{
+            pq.push(x);
+        }
+    }
+    return 0;
+}
